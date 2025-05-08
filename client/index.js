65b7addbde957f04c3764148c2a1a7dd5ff9b8bf
crismariudenis <deniscrismariu@gmail.com>
2025-05-08 17:38:34 +0300

    some navbar stuff

---
+++ client/index.js
@@ -0,0 +1,12 @@
+fetch("./components/navbar.html") // Fetch the navbar HTML file
+  .then((response) => response.text()) // Convert the response to text
+  .then((data) => {
+    document.getElementById("navbar").innerHTML = data; // Insert the content into the placeholder
+  });
+
+
+  fetch("./components/footer.html") // Fetch the navbar HTML file
+    .then((response) => response.text()) // Convert the response to text
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data; // Insert the content into the placeholder
+    });
